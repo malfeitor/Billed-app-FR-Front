6048a3f591d067213d4973911dc85c0a5a27ffdd
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2023-12-07 16:14:49 +0100

    test(Bills): every eye icon should show the bill

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -8,17 +8,21 @@ import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 import { ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
+import {mockedBills} from '../__mocks__/store.js'
+import userEvent from '@testing-library/user-event'
 
 import router from "../app/Router.js";
+import Bills from '../containers/Bills.js';
+import $ from 'jquery'
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
-
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee'
       }))
+
       const root = document.createElement("div")
       root.setAttribute("id", "root")
       document.body.append(root)
@@ -26,15 +30,37 @@ describe("Given I am connected as an employee", () => {
       window.onNavigate(ROUTES_PATH.Bills)
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
-      //to-do write expect expression
+
       expect(windowIcon).toHaveClass('active-icon')
     })
+
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
+    })
+
+    test('Then eye icon should show the bill', () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      const billsPage = new Bills({document, onNavigate, mockedBills, localStorageMock})
+      const mockHandleClickIconEye = jest.fn(billsPage.handleClickIconEye)
+      const modalFile = document.querySelector('#modaleFile')
+      $.fn.modal = jest.fn(modalFile.classList.add('show'))
+
+      const iconEyes = screen.getAllByTestId('icon-eye')
+      iconEyes.forEach(icon => {
+        icon.addEventListener('click', () => mockHandleClickIconEye(icon))
+      }) 
+      iconEyes.forEach(icon => {
+        userEvent.click(icon)
+      })
+
+      expect(mockHandleClickIconEye).toHaveBeenCalledTimes(iconEyes.length)
+      expect(modalFile).toHaveClass('show')
     })
   })
 })
